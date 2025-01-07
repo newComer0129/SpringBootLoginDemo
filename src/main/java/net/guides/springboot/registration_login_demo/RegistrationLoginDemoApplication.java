713f6d9829dc20801f4b3d4502eb1b07d0a0a414
david <david@daviddeMacBook-Pro.local>
2025-01-07 14:15:43 +0800

    upload all project

---
+++ src/main/java/net/guides/springboot/registration_login_demo/RegistrationLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot.registration_login_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
+	}
+
+}
